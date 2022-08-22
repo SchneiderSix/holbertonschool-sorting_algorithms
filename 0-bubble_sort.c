2bdd7d4d38f3943f3ba59bcdc6ac8a3bf132a775
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-22 20:02:05 -0300

    Task zero fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,10 +18,12 @@ void bubble_sort(int *array, size_t size)
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
+			{
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
-			print_array(array, size);
+				print_array(array, size);
+			}
 		}
 	}
 	printf("\n");
