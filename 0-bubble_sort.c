0056e4446e815626e6c5558b3692667393fc8aef
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-22 19:58:22 -0300

    Task zero

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,28 @@
+#include "sort.h"
 
+/**
+* bubble_sort - sort numbers from smallest to largest
+* @array: explicit name
+* @size: explicit name
+* Return: nothing
+*/
+void bubble_sort(int *array, size_t size)
+{
+	int i, j, tmp;
+
+	print_array(&array, &size);
+	printf("\n");
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+			print_array(&array, &size);
+		}
+	}
+	printf("\n");
+	print_array(&array, &size);
+}
