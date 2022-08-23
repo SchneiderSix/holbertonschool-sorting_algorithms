fb500f0c9e19f48deef91441ffeddebde5b54364
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-23 15:03:45 -0300

    task one

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,1 +1,20 @@
+#include "sort.h"
 
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current;
+	int tmp;
+
+	if (!list && !*list)
+		return;
+	while ((current = current->next))
+	{
+		if (current->n > current->next->n)
+		{
+			tmp = current->n;
+			current->n = current->next->n;
+			current->next->n = tmp;
+			print_list(*list);
+		}
+	}
+}
