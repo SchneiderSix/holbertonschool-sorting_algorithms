8fe80e7aa1b7dddfd08c90f6f9a3f30d15a834a3
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-23 18:23:22 -0300

    update task one

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,6 +7,15 @@
 * Return: nothing
 */
 void insertion_sort_list(listint_t **list)
+#include "sort.h"
+
+/**
+* insertion_sort_list - change place
+* of node min to max
+* @list: node
+* Return: nothing
+*/
+void insertion_sort_list(listint_t **list)
 {
 	listint_t *nex;
 
@@ -16,27 +25,27 @@ void insertion_sort_list(listint_t **list)
 	nex = (*list)->next;
 	while (nex)
 	{
-		while ((*nex)->prev && (*nex)->prev->n > (*nex)->n)
+		while (nex->prev && nex->prev->n > nex->n)
 		{
-			(*nex)->prev->next = (*nex)->next;
+			nex->prev->next = nex->next;
 
-			if ((*nex)->next)
-				(*nex)->next->prev = (*nex)->prev;
+			if (nex->next)
+				nex->next->prev = nex->prev;
 
-			(*nex)->next = (*nex)->prev;
+			nex->next = nex->prev;
 
-			if ((*nex)->next)
+			if (nex->next)
 			{
-				(*nex)->prev = (*nex)->next->prev;
-				(*nex)->next->prev = (*nex);
+				nex->prev = nex->next->prev;
+				nex->next->prev = nex;
 			}
 
-			if (!(*nex)->prev)
-				*list = (*nex);
+			if (!nex->prev)
+				*list = nex;
 			else
-				(*nex)->prev->next = (*nex);
+				nex->prev->next = nex;
 			print_list(*list);
 		}
-		nex = (*nex)->next;
+		nex = nex->next;
 	}
 }
