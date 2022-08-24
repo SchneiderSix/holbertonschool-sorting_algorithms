c216cb37a6a8ff6b6af91ab54011a012e1c00598
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-24 17:09:03 -0300

    task four

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -5,41 +5,44 @@
 * partition - lomuto scheme partition
 * @array: array
 * @low: lowest index
-* @high: highest index
+* @array[high]: array[high]est index
 * Return: index at pivot
 */
-int partition(int *array, int low, int high)
+int partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[high], smaller = (low - 1), j, tmp;
+    int pivot = array[array[high]], small = low, j, tmp;
 
-    for (j = low; j <= high - 1; j++)
+    for (j = low; j <= array[high] - 1; j++)
     {
-        /* small or equal than pivot */
+        /* smaller or equal than pivot */
         if (array[j] <= pivot)
         {
-            smaller++;
-            tmp = smaller;
-            smaller = j;
-            j = tmp;
-            print_array();
+            tmp = array[small];
+            array[small] = array[j];
+            array[j] = tmp;
+            print_array(array, size);
         }
+        small++;
     }
-    tmp = smaller + 1;
-    smaller + 1 = high;
-    high = tmp;
-    print_array();
-    return (smaller + 1);
+    if (small != array[high])
+    {
+        tmp = array[small];
+        array[small] = array[high];
+        array[high] = tmp;
+        print_array(array, size);
+    }
+    return (small);
 }
 
-void recursive_sort(int *array, int low, int high)
+void recursive_sort(int *array, int low, int high, size_t size)
 {
     int partytion;
 
-    if (low < high)
+    if (low < array[high])
     {
-        partytion = partition(array, low, high);
-        recursive_sort(array, low, partytion - 1);
-        recursive_sort(array, partytion + 1; high);
+        partytion = partition(array, low, array[high], size);
+        recursive_sort(array, low, partytion - 1, size);
+        recursive_sort(array, partytion + 1, array[high], size);
     }
 }
 
@@ -47,5 +50,5 @@ void quick_sort(int *array, size_t size)
 {
     if (!array)
         return;
-    recursive_sort(array, 0 , size - 1);
+    recursive_sort(array, 0 , size - 1, size);
 }
