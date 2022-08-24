a15638279233ea9a5ace329e1e6f8b1b257f974d
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-08-24 17:18:41 -0300

    fix task 4

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -5,50 +5,62 @@
 * partition - lomuto scheme partition
 * @array: array
 * @low: lowest index
-* @array[high]: array[high]est index
+* @high: highest index
 * Return: index at pivot
 */
 int partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[array[high]], small = low, j, tmp;
+	int pivot = array[high], small = low, j, tmp;
 
-    for (j = low; j <= array[high] - 1; j++)
-    {
-        /* smaller or equal than pivot */
-        if (array[j] <= pivot)
-        {
-            tmp = array[small];
-            array[small] = array[j];
-            array[j] = tmp;
-            print_array(array, size);
-        }
-        small++;
-    }
-    if (small != array[high])
-    {
-        tmp = array[small];
-        array[small] = array[high];
-        array[high] = tmp;
-        print_array(array, size);
-    }
-    return (small);
+	for (j = low; j <= array[high] - 1; j++)
+	{
+		/* smaller or equal than pivot */
+		if (array[j] <= pivot)
+		{
+			if (small != j)
+			{
+				tmp = array[small];
+				array[small] = array[j];
+				array[j] = tmp;
+				print_array(array, size);
+			}
+		}
+		small++;
+	}
+	if (small != high)
+	{
+		tmp = array[small];
+		array[small] = array[high];
+		array[high] = tmp;
+		print_array(array, size);
+	}
+	return (small);
 }
 
+/**
+* recursive_sort - recursive function to partition
+* depending on pivot 
 void recursive_sort(int *array, int low, int high, size_t size)
 {
-    int partytion;
+	int partytion;
 
-    if (low < array[high])
-    {
-        partytion = partition(array, low, array[high], size);
-        recursive_sort(array, low, partytion - 1, size);
-        recursive_sort(array, partytion + 1, array[high], size);
-    }
+	if (low < high)
+	{
+		partytion = partition(array, low, high, size);
+		recursive_sort(array, low, partytion - 1, size);
+		recursive_sort(array, partytion + 1, high, size);
+	}
 }
 
+/**
+* quick_sort - sort array in asc
+* @array: explicit name
+* @size: explicit name
+* Return: nothing
+*/
 void quick_sort(int *array, size_t size)
 {
-    if (!array)
-        return;
-    recursive_sort(array, 0 , size - 1, size);
+	if (!array)
+		return;
+	recursive_sort(array, 0 , size - 1, size);
 }
